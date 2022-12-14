3a1c36469b2d2944d8321a79ba6a221f186a4e2c
Anton Ovinnikov <toshakins@gmail.com>
2022-12-14 18:45:45 +0200

    * Allow overriding printer function * Drop support Py 3.6 * Add support Py 3.10, 3.11

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,15 +1,14 @@
 import unittest
-from unittest.mock import call
+from unittest.mock import call, Mock
 
 from freezegun import freeze_time
 
 from tepet import Timer
-from tests.utils import PatchMixin
 
 
-class TimerTest(unittest.TestCase, PatchMixin):
+class TimerTest(unittest.TestCase):
     def setUp(self):
-        self.printer_mock = self._patch('tepet.timer._printer')
+        self.printer_mock = Mock()
 
     def assertCallsPrinterCorrectly(self):
         calls = [
@@ -20,13 +19,14 @@ class TimerTest(unittest.TestCase, PatchMixin):
 
     def test_timer_works_as_a_contextmanager(self):
         with freeze_time("1970-01-01"):
-            with Timer():
-                pass
+            with Timer(printer=self.printer_mock):
+                pass # noqa
         self.assertCallsPrinterCorrectly()
 
     def test_timer_works_as_a_decorator(self):
-        @Timer()
+        @Timer(printer=self.printer_mock)
         def workload():
+            # noqa
             pass
 
         with freeze_time("1970-01-01"):
--- tests/utils.py
+++ tests/utils.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 class PatchMixin(object):
     def _patch(self, *args, **kwargs):
-        patcher = patch(*args, **kwargs)
+        patcher = patch.object(*args, **kwargs)
         patched_entity = patcher.start()
         self.addCleanup(patcher.stop)
         return patched_entity
